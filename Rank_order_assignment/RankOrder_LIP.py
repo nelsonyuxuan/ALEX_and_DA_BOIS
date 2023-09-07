740f2767075cf2e863ccd84bfb891823a51f950f
Sai Koukuntla <sai.koukunt@gmail.com>
2023-09-07 00:37:04 -0400

    Added demo, support for different capacities

--- Rank_order_assignment/RankOrder_LIP.py
+++ Rank_order_assignment/RankOrder_LIP.py
@@ -4,29 +4,29 @@ Created on Tue Aug 29 16:52 2023
 Using the Integer Linear Programming
 
 The function in this file is used to matches N patients with K doctors. Each patient is allowed to provide a ranked list 
-of their ppreference for doctors, however, doctors are rohibited from displaying preferences for patinets. Thus the code should takes in the following:
+of their preference for doctors, however, doctors are prohibited from displaying preferences for patients. Thus the code should takes in the following:
 
-- A list of ranked preferences, 1 list for each patient.
-- A maximum capacity for each doctor (can initially assume the same capacity - note that the total capacity should exceed the number of patients)
+- A 2D list of ranked preferences (# of patients X # of doctors) (highest number = most preferred).
+- A list of the maximum capacity for each doctor (assume that the total capacity exceeds the number of patients)
 
 Output Return:
-
 - A list of assignments indicating which doctors are to take care of which patients
 
 Assumptions:
 @ 09/04/2023:
 - Doctors don't have preferences for patients (since they are not displaying their preferences)
-- Doctors have the same capacity 
-- The list of preferences are assumed to be no equals (i.e. no ties). The maximum preference would be equal to the number of doctors (i.e. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 for 10 doctors).
+- Total capacity exceeds the number of patients
+- Each patient provides a rank for every doctor
+- Ties are allowed
 
 Summary of the function:
 Consider the problem as the Integer Linear Problem. Define the binary variables x_ij that are equal to 1 when patient i is assigned to doctor j and 0 otherwise. 
 The objective function is to maximize the sum of preferences. The constraints are that each patient is assigned to exactly one doctor and each doctor has a maximum capacity.
 
-Author: @Nelson Wu 
+Author: @Nelson Wu, @Sai Koukuntla, @Alexandra Cheng 
 '''
 
-from pulp import LpMaximize, LpProblem, LpVariable, lpSum
+from pulp import LpMaximize, LpProblem, LpVariable, lpSum, PULP_CBC_CMD
 
 def ilp_assignment(preferences, doctor_capacity):
     num_patients = len(preferences)
@@ -39,7 +39,8 @@ def ilp_assignment(preferences, doctor_capacity):
     x = {}
     for i in range(num_patients):
         for j in range(num_doctors):
-            x[i, j] = LpVariable(name=f"x_{i}_{j}", cat="Binary")
+            for k in range(doctor_capacity[j]):
+                x[i, j] = LpVariable(name=f"x_{i}_{j}", cat="Binary")
 
     # Objective function: Maximize the sum of preferences
     model += lpSum(preferences[i][j] * x[i, j] for i in range(num_patients) for j in range(num_doctors)), "Total_Preference"
@@ -50,10 +51,11 @@ def ilp_assignment(preferences, doctor_capacity):
 
     # Constraint: Each doctor has a maximum capacity
     for j in range(num_doctors):
-        model += lpSum(x[i, j] for i in range(num_patients)) <= doctor_capacity, f"Doctor_{j}"
+        for k in range(doctor_capacity[j]):
+            model += lpSum(x[i, j] for i in range(num_patients)) <= doctor_capacity[j], f"Doctor_{j}_{k}"
 
     # Solve the problem
-    model.solve()
+    model.solve(PULP_CBC_CMD(msg=0))
 
     # Extract the assignments
     assignments = [[] for _ in range(num_doctors)]
@@ -64,14 +66,14 @@ def ilp_assignment(preferences, doctor_capacity):
 
     return assignments
 
-# Test the function
-preferences = [
-    [5, 2, 1],
-    [4, 3, 1],
-    [3, 2, 4],
-    [1, 5, 2]
-]
-doctor_capacity = 2
+# # Test the function
+# preferences = [
+#     [5, 2, 1],
+#     [4, 3, 1],
+#     [3, 2, 4],
+#     [1, 5, 2]
+# ]
+# doctor_capacity = 2
 
-assignments = ilp_assignment(preferences, doctor_capacity)
-print("Assignments:", assignments)
+# assignments = ilp_assignment(preferences, doctor_capacity)
+# print("Assignments:", assignments)
